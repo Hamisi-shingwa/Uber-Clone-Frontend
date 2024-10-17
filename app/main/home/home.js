8d162d605211d9bb8df5fd12b31df79f31296aed
Hamisi Faraji Hamisi <hamisifaraji369@gmail.com>
2024-10-17 18:10:27 -0400

    Configuration of Picklocation screen on list of home stack

--- app/main/home/home.js
+++ app/main/home/home.js
@@ -1,14 +1,13 @@
 // This file arrange all available files on home folder and create it into stack
-//Some of screen at this stack link with screen available on service tab
-//this is so why am try to import Service screen inorder to enable react to get reference nvigation route of service
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeInterface } from './homeinterface';
 import { Promos } from './promo';
-import { Service } from '../service/service';
 import PlanReserve from './homereserve/plan_reserve';
 import { ComfirmReserve } from './homereserve/confirm';
 import TimelyReserve from './homereserve/timely_reserve';
+import PlanRide from '../service/plan_ride';
+import PickLocation from './pick_location';
 
 const {Screen, Navigator} = createNativeStackNavigator()
 
@@ -25,10 +24,11 @@ export  function Home(){
           >
             <Screen name="homeinterface" component={HomeInterface}/>
             <Screen name="promos" component={Promos}/>
+            <Screen name="pickLocation" component={PickLocation}/>
             <Screen name="planreserve" component={PlanReserve}/>
             <Screen name="comfirmreserve" component={ComfirmReserve}/>
-            <Screen name="service" component={Service}/>
             <Screen name="timelyReserve" component={TimelyReserve}/>
+            <Screen name="planride" component={PlanRide}/>
           </Navigator>
      
     )
