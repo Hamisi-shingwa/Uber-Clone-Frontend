38c5e15ac5a229d17bf4a26bf0ab877d3ecb18a7
Hamisi Faraji Hamisi <hamisifaraji369@gmail.com>
2024-10-17 18:38:06 -0400

    Creation of file that use NetInfo to check device network status

---
+++ components/welcome/check_conection.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import NetInfo from '@react-native-community/netinfo';
+
+const CheckConnection = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isConnected, setIsConnected] = useState<boolean>(false); 
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(!!state.isConnected); 
+      setIsLoading(false);
+    });
+
+   
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      if (isConnected) {
+        // navigation.navigate('NextScreen'); 
+      } else {
+        Alert.alert('No Internet Connection', 'Please check your internet connection and try again.');
+      }
+    }
+  }, [isLoading, isConnected, navigation]);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : null}
+      {!isLoading && !isConnected && (
+        <Text>No internet connection. Please check your connection.</Text>
+      )}
+    </View>
+  );
+};
+
+export default CheckConnection;
